cbd6c8078e16000a8954fc764a93a10fb126707b
yaswanthyadavchatala <yaswanthvisoli@gmail.com>
2022-10-08 11:47:25 +0530

    added file

---
+++ src/main/java/emahabhoomiproject/App.java
@@ -0,0 +1,13 @@
+package emahabhoomiproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
